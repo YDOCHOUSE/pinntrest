c760d6d1cac77c850cfac6594ba88394be76ad67
Cody House <codyahouse@gmail.com>
2015-04-15 13:44:09 -0400

    included description and tags in search params

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,8 +3,10 @@ class Pin < ActiveRecord::Base
   acts_as_taggable
   acts_as_votable
   belongs_to :user
-  searchkick
+  searchkick word_start: [:name, :description]
 
   has_attached_file :image, styles: { :medium => "300x300>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
 end
