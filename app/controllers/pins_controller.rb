6465e6de261f1f6529a5df948112d3c9e16c34af
Cody House <codyahouse@gmail.com>
2015-04-14 20:48:18 -0400

    created, show, edit, and update actions and views for pins, minor changes to index, and new actions as well

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,8 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pins = Pin.all.order(created_at: :desc)
   end
 
   def new
@@ -8,12 +11,40 @@ class PinsController < ApplicationController
 
   def create
   	@pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: "Pin Created!"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Your Pin has been updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    
   end
 
 
   private
 
   def pin_params
-  	params.require(:title, :description)
+  	params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 end
